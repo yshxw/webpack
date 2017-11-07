4f880c08b80db17fb5cd0d735dca9af4f6fe3dce
yugasun <yuga_sun@163.com>
2017-11-07 17:14:24 +0800

    Add mobile develop feature

--- meta.js
+++ meta.js
@@ -1,92 +1,110 @@
 module.exports = {
-  "helpers": {
-    "if_or": function (v1, v2, options) {
+  'helpers': {
+    'if_or': function (v1, v2, options) {
       if (v1 || v2) {
-        return options.fn(this);
+        return options.fn(this)
       }
 
-      return options.inverse(this);
+      return options.inverse(this)
     }
   },
-  "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "message": "Project name"
+  'prompts': {
+    'name': {
+      'type': 'string',
+      'required': true,
+      'message': 'Project name'
     },
-    "description": {
-      "type": "string",
-      "required": false,
-      "message": "Project description",
-      "default": "A Vue.js project"
+    'description': {
+      'type': 'string',
+      'required': false,
+      'message': 'Project description',
+      'default': 'A Vue.js project'
     },
-    "author": {
-      "type": "string",
-      "message": "Author"
+    'author': {
+      'type': 'string',
+      'message': 'Author'
     },
-    "build": {
-      "type": "list",
-      "message": "Vue build",
-      "choices": [
+    'build': {
+      'type': 'list',
+      'message': 'Vue build',
+      'choices': [
         {
-          "name": "Runtime + Compiler: recommended for most users",
-          "value": "standalone",
-          "short": "standalone"
+          'name': 'Runtime + Compiler: recommended for most users',
+          'value': 'standalone',
+          'short': 'standalone'
         },
         {
-          "name": "Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere",
-          "value": "runtime",
-          "short": "runtime"
+          'name': 'Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere',
+          'value': 'runtime',
+          'short': 'runtime'
         }
       ]
     },
-    "router": {
-      "type": "confirm",
-      "message": "Install vue-router?"
-    },
-    "lint": {
-      "type": "confirm",
-      "message": "Use ESLint to lint your code?"
-    },
-    "lintConfig": {
-      "when": "lint",
-      "type": "list",
-      "message": "Pick an ESLint preset",
-      "choices": [
+    'platform': {
+      'type': 'list',
+      'message': 'Chose your platform',
+      'choices': [
         {
-          "name": "Standard (https://github.com/standard/standard)",
-          "value": "standard",
-          "short": "Standard"
+          'name': 'PC',
+          'value': 'pc'
         },
         {
-          "name": "Airbnb (https://github.com/airbnb/javascript)",
-          "value": "airbnb",
-          "short": "Airbnb"
-        },
-        {
-          "name": "none (configure it yourself)",
-          "value": "none",
-          "short": "none"
+          'name': 'Mobile',
+          'value': 'mobile'
         }
       ]
     },
-    "unit": {
-      "type": "confirm",
-      "message": "Setup unit tests with Karma + Mocha?"
+    'ionicons': {
+      'type': 'confirm',
+      'message': 'Use ionic icons? (https://github.com/ionic-team/ionicons)'
     },
-    "e2e": {
-      "type": "confirm",
-      "message": "Setup e2e tests with Nightwatch?"
+    'router': {
+      'type': 'confirm',
+      'message': 'Install vue-router?'
+    },
+    'lint': {
+      'type': 'confirm',
+      'message': 'Use ESLint to lint your code?'
+    },
+    'lintConfig': {
+      'when': 'lint',
+      'type': 'list',
+      'message': 'Pick an ESLint preset',
+      'choices': [
+        {
+          'name': 'Standard (https://github.com/standard/standard)',
+          'value': 'standard',
+          'short': 'Standard'
+        },
+        {
+          'name': 'Airbnb (https://github.com/airbnb/javascript)',
+          'value': 'airbnb',
+          'short': 'Airbnb'
+        },
+        {
+          'name': 'none (configure it yourself)',
+          'value': 'none',
+          'short': 'none'
+        }
+      ]
+    },
+    'unit': {
+      'type': 'confirm',
+      'message': 'Setup unit tests with Karma + Mocha?'
+    },
+    'e2e': {
+      'type': 'confirm',
+      'message': 'Setup e2e tests with Nightwatch?'
     }
   },
-  "filters": {
-    ".eslintrc.js": "lint",
-    ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "test/unit/**/*": "unit",
-    "build/webpack.test.conf.js": "unit",
-    "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router"
+  'filters': {
+    '.eslintrc.js': 'lint',
+    '.eslintignore': 'lint',
+    'config/test.env.js': 'unit || e2e',
+    'test/unit/**/*': 'unit',
+    'build/webpack.test.conf.js': 'unit',
+    'test/e2e/**/*': 'e2e',
+    'src/router/**/*': 'router'
   },
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
-};
+  'completeMessage': 'To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack'
+}
